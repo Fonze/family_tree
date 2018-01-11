85d88bf910cf3a10031b56f789222671bcc6c145
Fonze <egivic@gmail.com>
2018-01-11 18:09:57 +0200

    initial create project, add redux and navigation

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+import Index from './app';
+
+class App extends Component<{}> {
+  render() {
+    return (
+      <Index />
+    );
+  }
+}
+
+export default App;
